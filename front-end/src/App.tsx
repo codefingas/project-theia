1d1ff11dfd8be29270db35d2ddc4f4f8644e67cb
codefingas <udohezekiel34@yahoo.com>
2022-01-16 13:12:48 +0100

    merged server

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
-import { Card, Tabs, Tab } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Tabs, Tab, Spinner } from "react-bootstrap";
 import styled from "styled-components";
-import FileUploadComponent from './components/FileUploader';
+import FileUploadComponent from "./components/FileUploader";
 
 const CenterDiv = styled.div`
   margin: auto;
@@ -22,8 +22,8 @@ const GridDiv = styled.div`
   div:first-child {
     width: 100%;
   }
-  div:nth-child(2){
-   margin: 0 auto;
+  div:nth-child(2) {
+    margin: 0 auto;
   }
 `;
 
@@ -37,7 +37,9 @@ const App = () => {
             <Tab eventKey="Original" title="Original">
               <GridDiv className="grid-container">
                 <div className="grid-item">
-                <FileUploadComponent isUploaded={(value: boolean) => setProcessed(value)}/>
+                  <FileUploadComponent
+                    isUploaded={(value: boolean) => setProcessed(value)}
+                  />
                 </div>
                 <div className="grid-item">
                   {/* <Button variant="primary" disabled>
@@ -53,13 +55,45 @@ const App = () => {
                 </div>
               </GridDiv>
             </Tab>
-            <Tab eventKey="RemoveBG" title="Remove BG" disabled={processed}>
+            <Tab
+              eventKey="RemoveBG"
+              title={
+                !processed ? (
+                  <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                  />
+                ) : (
+                  `Remove BG`
+                )
+              }
+              disabled={processed}
+            >
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea
               deserunt alias aliquam animi quod sed ducimus, ex illo quis
               aliquid sequi libero nam beatae porro, provident sit quasi
               consequuntur a.
             </Tab>
-            <Tab eventKey="Add-Background" title="Add Background" disabled={processed}>
+            <Tab
+              eventKey="Add-Background"
+              title={
+                !processed ? (
+                  <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                  />
+                ) : (
+                  `Add Background`
+                )
+              }
+              disabled={processed}
+            >
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
               natus aliquam cumque atque minus? Ratione ex sit earum minima
               nostrum unde consequatur sed aliquid quam facere consectetur,
